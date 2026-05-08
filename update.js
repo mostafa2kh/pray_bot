852e57dedecb9aef910696270b641d5d584fd978
mostafa2kh <102628721+mostafa2kh@users.noreply.github.com>
2026-05-08 20:05:54 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,29 +1,27 @@
-import fetch from "node-fetch";
 import fs from "fs";
 
 const city = "Qalyubia";
 const country = "Egypt";
 
-// Aladhan API
 const url = `https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=5`;
 
 async function update() {
-  const res = await fetch(url);
+  const res = await fetch(url); // native fetch (NO PACKAGE NEEDED)
   const data = await res.json();
 
-  const timings = data.data.timings;
+  const t = data.data.timings;
 
   const prayerTimes = {
-    Fajr: timings.Fajr,
-    Dhuhr: timings.Dhuhr,
-    Asr: timings.Asr,
-    Maghrib: timings.Maghrib,
-    Isha: timings.Isha
+    Fajr: t.Fajr,
+    Dhuhr: t.Dhuhr,
+    Asr: t.Asr,
+    Maghrib: t.Maghrib,
+    Isha: t.Isha
   };
 
   fs.writeFileSync("prayer.json", JSON.stringify(prayerTimes, null, 2));
 
-  console.log("Prayer times updated:", prayerTimes);
+  console.log("Updated:", prayerTimes);
 }
 
 update();
